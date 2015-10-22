3d1355460911bdc1d7ec05cfcfe3fe83a3532980
Sergey Podgornyy <sergey.podgornyy@yahoo.de>
2015-10-23 02:13:13 +0300

    Layouts with Twig

--- index.php
+++ index.php
@@ -14,15 +14,25 @@ date_default_timezone_set('Europe/Kiev');
 // $log->pushHandler(new StreamHandler('app.log', Logger::WARNING));
 // $log->addWarning('Foo');
 
-$app = new \Slim\Slim();   //new slim object instance
+$app = new \Slim\Slim( array(
+	'view' => new \Slim\Views\Twig()
+));   //new slim object instance
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true
+);
+$view->parserExtensions = array(
+    new \Slim\Views\TwigExtension()
+);
 
 //define a HTTP GET route
 $app->get('/', function () use($app){
-    $app->render('index.html');
+    $app->render('about.twig');
 });
 
 $app->get('/contact', function () use($app){
-    $app->render('contact.html');
+    $app->render('contact.twig');
 });
 
 //Run the slim application
